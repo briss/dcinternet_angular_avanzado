f4d93af993969db3753725201dd1a6ec9fed9199
brissgs <briseidags@gmail.com>
2025-12-02 18:55:04 -0600

    Clase 3: Agregar ejemplos al menú

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { App } from './app';
 import { Busqueda } from './componentes/busqueda/busqueda';
 import { NotFound } from './componentes/not-found/not-found';
 import { Zona } from './componentes/zona/zona';
+import { Contenedor } from './componentes/signals/contenedor/contenedor';
+import { ApiResource } from './componentes/api-resource/api-resource';
 
 export const routes: Routes = [
     { path: '', component: App },
     { path: 'busqueda', component: Busqueda },
     { path: 'zona', component: Zona},
+    { path: 'contenedor', component: Contenedor },
+    { path: 'apiResource', component: ApiResource },
 
     { path: '**', component: NotFound }
 ];
