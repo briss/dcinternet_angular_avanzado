6d527afbd8b16c701eab3ec1080c927776912cd6
brissgs <briseidags@gmail.com>
2025-12-10 18:56:37 -0600

    Clase 5: Mostrar lista de cursos

--- src/app/componentes/busqueda/busqueda-bd/busqueda-bd.ts
+++ src/app/componentes/busqueda/busqueda-bd/busqueda-bd.ts
@@ -1,14 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { ListaCurso } from "../lista-curso/lista-curso";
 import { ServicioCursos } from '../../../services/servicio-cursos';
 import { Observable } from 'rxjs';
 import { Curso } from '../../../modelo/curso';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-busqueda-bd',
-  imports: [ReactiveFormsModule, ListaCurso, AsyncPipe, JsonPipe],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './busqueda-bd.html',
   styleUrl: './busqueda-bd.css',
 })
@@ -16,14 +15,14 @@ export class BusquedaBD {
 
   servicio = inject(ServicioCursos);
   busqueda:FormControl<string | null> = new FormControl('');
-  
-  cursos$ = Observable<Curso[]>;
+
+  cursos$:Observable<Curso[]> = this.servicio.buscarCurso('');
 
   terminoBusqueda = "";
 
   constructor() {
     this.busqueda.valueChanges.subscribe(value => {
-      this.cursos$ = this.servicio.buscarCurso(value ?? '').subscribe;
+      this.cursos$ = this.servicio.buscarCurso(value ?? '');
     });
   }
 }
