4eed95923fddf788a5a81663caff6146f5aaac6e
brissgs <briseidags@gmail.com>
2026-01-06 13:16:04 -0600

    Clase 8: Agregar componente para consultar cuentas del usuario autentificado

---
+++ src/app/componentes/auth/cuentas/cuentas.ts
@@ -0,0 +1,23 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth-service';
+
+@Component({
+  selector: 'app-cuentas',
+  imports: [],
+  templateUrl: './cuentas.html',
+  styleUrl: './cuentas.css',
+})
+export class Cuentas implements OnInit {
+  authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.authService.getCuentas().subscribe({
+      next: cuentas => {
+        console.log(cuentas)
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+}
