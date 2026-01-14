df42b5ee1f3ed8edc06d5b1b4165e907ba508ece
brissgs <briseidags@gmail.com>
2026-01-13 19:58:54 -0600

    Clase 10: Agregar componentes relacionado padre - hijo para ejemplo con guards

---
+++ src/app/componentes/auth-parent/padre/padre.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-padre',
+  imports: [ RouterModule ],
+  templateUrl: './padre.html',
+  styleUrl: './padre.css',
+})
+export class Padre {
+
+}
